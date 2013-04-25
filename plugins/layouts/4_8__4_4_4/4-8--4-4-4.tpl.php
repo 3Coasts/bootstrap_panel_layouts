77743db050457cb3268e95239df34b58e477ecda
Brian Berlin <bri@nberl.in>
2013-04-25 13:20:20 -0500

    updates and added new layout

---
+++ plugins/layouts/4_8__4_4_4/4-8--4-4-4.tpl.php
@@ -0,0 +1,21 @@
+<div class='container'>
+	<div class='row'>
+		<div class='span4'>
+			<?php print $content['top_left']; ?>
+		</div>
+		<div class='span8'>
+			<?php print $content['top_right']; ?>
+		</div>
+	</div>
+	<div class='row'>
+		<div class='span4'>
+			<?php print $content['bottom_left']; ?>
+		</div>
+		<div class='span4'>
+			<?php print $content['bottom_middle']; ?>
+		</div>
+		<div class='span4'>
+			<?php print $content['bottom_right']; ?>
+		</div>
+	</div>
+</div>
