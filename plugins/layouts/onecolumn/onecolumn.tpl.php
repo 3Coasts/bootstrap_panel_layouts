fcfbd01a9216de42db119565a6b574ad4353e9cb
Brian Berlin <bri@nberl.in>
2013-08-27 12:26:28 -0500

    fixed one column to only show sections that are used. should do this to the others

--- plugins/layouts/onecolumn/onecolumn.tpl.php
+++ plugins/layouts/onecolumn/onecolumn.tpl.php
@@ -1,5 +1,25 @@
 <div class='onecolumn'>
-  <div class='header'><div class='inside'><?php print $content['header']; ?></div></div>
-  <div class='content'><div class='inside'><?php print $content['center']; ?></div></div>
-  <div class='footer'><div class='inside'><?php print $content['footer']; ?></div></div>
+  <?php if(!empty($content['header'])): ?>  
+  <div class='header'>
+    <div class='inside'>
+      <?php print $content['header']; ?>
+    </div>
+  </div>
+  <?php endif; ?>
+  
+  <?php if(!empty($content['center'])): ?>
+  <div class='content'>
+    <div class='inside'>
+      <?php print $content['center']; ?>
+    </div>
+  </div>
+  <?php endif; ?>
+  
+  <?php if(!empty($content['footer'])): ?>
+  <div class='footer'>
+    <div class='inside'>
+      <?php print $content['footer']; ?>
+    </div>
+  </div>
+<?php endif; ?>
 </div>
