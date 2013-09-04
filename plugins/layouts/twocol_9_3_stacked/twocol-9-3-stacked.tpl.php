d038bb4f1dc486d2cca9483e2fd170a5c8f9ab71
Brian Berlin <bri@nberl.in>
2013-09-04 09:28:02 -0500

    fixed clearing problem with twocol-9-3

--- plugins/layouts/twocol_9_3_stacked/twocol-9-3-stacked.tpl.php
+++ plugins/layouts/twocol_9_3_stacked/twocol-9-3-stacked.tpl.php
@@ -2,14 +2,16 @@
 	<div class="header">
   	<?php print $content['header']; ?>
 	</div>
+  
+  <div class='wrapper'>
+  	<div class="left">
+    	<?php print $content['left']; ?>
+  	</div>
 
-	<div class="left">
-  	<?php print $content['left']; ?>
-	</div>
-
-	<div class="right">
-  	<?php print $content['right']; ?>
-	</div>
+  	<div class="right">
+    	<?php print $content['right']; ?>
+  	</div>
+  </div>
 
 	<div class="footer">
     <?php print $content['footer']; ?>
